00d33bc80761e43d2877dff62cd3b36f63ec1d61
Joaquim Cardeira <asklocation.net@gmail.com>
2025-05-19 12:35:25 +0000

    first

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -x
+
+# change directory to the where you would like to install Pelias
+# cd /path/to/install
+
+# clone this repository
+git clone https://github.com/jcardus/pelias-docker.git && cd pelias-docker
+
+# install pelias script
+# this is the _only_ setup command that should require `sudo`
+sudo ln -s "$(pwd)/pelias" /usr/local/bin/pelias
+
+# cd into the project directory
+cd projects/brazil
+
+# create a directory to store Pelias data files
+# see: https://github.com/pelias/docker#variable-data_dir
+# note: use 'gsed' instead of 'sed' on a Mac
+mkdir ./data
+sed -i '/DATA_DIR/d' .env
+echo 'DATA_DIR=./data' >> .env
+
+# run build
+pelias compose pull
+pelias elastic start
+pelias elastic wait
+pelias elastic create
+pelias download all
+pelias prepare all
+pelias import all
+pelias compose up
+
+# optionally run tests
+pelias test run
