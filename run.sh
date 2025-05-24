e52c397a5113fc0006883f39ebf947ca97516848
jcardus <joaquim_cardeira@hotmail.com>
2025-05-24 05:45:35 +0100

    merge csv

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -x
 
 # cd into the project directory
-cd projects/frotaweb
+cd projects/frotaweb || exit
 
 # create a directory to store Pelias data files
 # see: https://github.com/pelias/docker#variable-data_dir
@@ -21,9 +21,12 @@ pelias download wof
 pelias download oa
 
 mkdir -p ./data/polylines
-cd data/polylines
+cd data/polylines || exit
 wget https://data.geocode.earth/osm/2022-35/brazil-valhalla.polylines.0sv.gz
 gunzip brazil-valhalla.polylines.0sv.gz
+wget https://github.com/jcardus/pelias-docker/raw/refs/heads/master/projects/frotaweb/polylines-cl-pt.sv0
+cat brazil-valhalla.polylines.0sv polylines-cl-pt.sv0 > polylines.sv0
+rm brazil-valhalla.polylines.0sv polylines-cl-pt.sv0
 cd ../..
 
 pelias import polylines
